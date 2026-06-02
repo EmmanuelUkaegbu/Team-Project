9dabcc7c6eb77e249c90d661e8936388c9e7bdc7
unknown <sklasho08@gmail.comgit config --global user.name>
2026-06-02 09:30:10 +0100

    added call-section

--- src/Component/Call.tsx
+++ src/Component/Call.tsx
@@ -1,5 +1,18 @@
+import "./Call.css";
 const Call = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section className="cta">
+        <h1>
+          Enroll for our various online courses <br />
+          Anywhere from The World
+        </h1>
+        <a href="" className="hero-btn">
+          CONTACT US
+        </a>
+      </section>
+    </div>
+  );
 };
 
 export default Call;
