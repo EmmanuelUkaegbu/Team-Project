8bcd8190981ee33f320289a0f390ebf86e7572bd
danielmunaboss <abrahamdanielmunachi@gmail.com>
2026-06-08 13:20:14 -0700

    blog-page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import Blog from "./Pages/Blog";
 const App = () => {
   return (
     <div>
       <Routes>
-       
-         <Route path="/" element={<Login />} />
- <Route path="/home" element={<Home />} />
-  <Route path="/register" element={<Register />} />
-
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </div>
   );
